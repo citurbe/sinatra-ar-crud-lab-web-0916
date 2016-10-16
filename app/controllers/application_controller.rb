bcb8516aa67a8d780736298a943462f403da2eda
Craig Iturbe <citurbe@gmail.com>
2016-10-16 14:44:08 -0400

    create basic blog system, everything works but capybara is failing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,45 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
+  get '/' do
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :'index.html'
+  end
+
+  get '/posts/new' do
+    erb :'new.html'
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :'show.html'
+  end
+
+  post '/posts' do
+    Post.create(name: params[:name], content: params[:content]).save
+    redirect '/posts'
+  end
+
+  get '/posts/:id/update' do
+    @post = Post.find(params[:id])
+    erb :'update.html'
+  end
+
+  delete '/posts/:id/delete' do
+    @name = Post.find(params[:id]).name
+    Post.destroy(params[:id])
+    erb :'deleted.html'
+  end
+
+  patch '/posts/:id' do
     
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    @post.save
+    redirect '/posts'
   end
 end
---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ActiveRecord::Base
+  # @@posts
+  #
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  #   @@posts << self
+  # end
+  #
+  # def self.all
+  #   @@posts
+  # end
+
+end
